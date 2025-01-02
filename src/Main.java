9774bbbb1c97659c67f125e68628963faca13616
sebastian <sebastianp136@nycstudents.net>
2025-01-02 10:10:57 -0500

    changed print statement

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
         System.out.println("our first shared repo");
+        System.out.print("Hello");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
